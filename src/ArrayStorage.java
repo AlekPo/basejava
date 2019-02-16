df792cd2888f69729d2ef359d1daafbfab7e199e
AlekPo <al0do@yandex.ru>
2019-02-16 12:05:46 +0300

    Correction of remark 1 in homework 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,34 +5,33 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
+    private int tempSize;
 
     void clear() {
-        int tempSize = size();
-        for (int i = 0; i < tempSize; i++) {
-            Arrays.fill(storage, 0, tempSize, null);
-        }
+        Arrays.fill(storage, 0, tempSize, null);
+        tempSize = 0;
     }
 
     void save(Resume r) {
-        int tempSize = size();
         if (tempSize == storage.length) {
             System.out.println("Error - the resume database is full");
         } else if (tempSize == 0) {
             storage[tempSize] = r;
+            tempSize++;
         } else {
-            for (int i = 0; i < tempSize; i++) {
+            for (int i = 0; i < tempSize; i++)
                 if (storage[i].uuid.equals(r.uuid)) {
                     System.out.println("Error - Resume with uuid '" + r.uuid + "' already have");
                     break;
                 } else if (i + 1 == tempSize) {
                     storage[tempSize] = r;
+                    tempSize++;
+                    break;
                 }
-            }
         }
     }
 
     Resume get(String uuid) {
-        int tempSize = size();
         for (int i = 0; i < tempSize; i++) {
             if (uuid.equals(storage[i].uuid)) {
                 return storage[i];
@@ -43,14 +42,16 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int tempSize = size();
         for (int i = 0; i < tempSize; i++) {
             if (uuid.equals(storage[i].uuid)) {
                 if (tempSize - 1 - i >= 0) {
                     System.arraycopy(storage, i + 1, storage, i, tempSize - 1 - i);
                 }
                 storage[tempSize - 1] = null;
+                tempSize--;
                 break;
+            } else if (i + 1 == tempSize) {
+                System.out.println("Error - No resume with uuid '" + uuid);
             }
         }
     }
@@ -59,19 +60,19 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int tempSize = size();
         Resume[] tempStorage = new Resume[tempSize];
         System.arraycopy(storage, 0, tempStorage, 0, tempSize);
         return tempStorage;
     }
 
     int size() {
-        int counter = 0;
-        for (Resume resCount : storage) {
-            if (resCount != null) {
-                counter++;
-            }
-        }
-        return counter;
+        return tempSize;
+//        int counter = 0;
+//        for (Resume resCount : storage) {
+//            if (resCount != null) {
+//                counter++;
+//            }
+//        }
+//        return counter;
     }
 }
