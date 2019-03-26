ba7859389794ba47e7ddc1c503656954f6d4c57b
AlekPo <al0do@yandex.ru>
2019-03-26 22:11:38 +0300

    Fix Note 1 in HW 4 - add methods

--- src/MainReflection.java
+++ src/MainReflection.java
@@ -5,16 +5,6 @@ import java.lang.reflect.Method;
 
 public class MainReflection {
     public static void main(String[] args) throws Exception {
-
-/**
- * Провалившаяся попытка изменения значения static final поля "STORAGE_LIMIT",
- * final поле невозможно изменить, т.к. значение присваивается сразу на этапе компиляции.
- */
-//        Field modifiersField = AbstractArrayStorage.class.getDeclaredField("STORAGE_LIMIT");
-//        modifiersField.setAccessible(true);
-//        modifiersField.set(null, 10);
-//        System.out.println(modifiersField.get(null));
-
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         System.out.println(field.getName());
--- test/storage/AbstractArrayStorageTest.java
+++ test/storage/AbstractArrayStorageTest.java
@@ -9,24 +9,19 @@ import org.junit.Before;
 import org.junit.Test;
 
 public abstract class AbstractArrayStorageTest {
+    private Storage storage;
+    private static final String UUID_1 = "uuid1";
+    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final String UUID_2 = "uuid2";
+    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final String UUID_3 = "uuid3";
+    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final String UUID_4 = "uuid4";
+    private static final Resume RESUME_4 = new Resume(UUID_4);
 
     protected AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
     }
-
-    private Storage storage;
-
-    private static final String UUID_1 = "uuid1";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
-
-    private static final String UUID_2 = "uuid2";
-    private static final Resume RESUME_2 = new Resume(UUID_2);
-
-    private static final String UUID_3 = "uuid3";
-    private static final Resume RESUME_3 = new Resume(UUID_3);
-
-    private static final String UUID_4 = "uuid4";
-    private static final Resume RESUME_4 = new Resume(UUID_4);
 
     @Before
     public void setUp() {
@@ -60,6 +55,7 @@ public abstract class AbstractArrayStorageTest {
     @Test
     public void getAll() {
         Assert.assertArrayEquals(new Resume[]{RESUME_1, RESUME_2, RESUME_3}, storage.getAll());
+        Assert.assertEquals(3, storage.size());
     }
 
     @Test
@@ -91,12 +87,21 @@ public abstract class AbstractArrayStorageTest {
     public void delete() {
         storage.delete(UUID_3);
         Assert.assertEquals(2, storage.size());
-
-        storage.delete("dummy");
+        storage.get(UUID_3);
     }
 
     @Test(expected = NotExistStorageException.class)
+    public void deleteNotExist() {
+        storage.delete("dummy");
+    }
+
+    @Test
     public void update() {
+        storage.update(RESUME_3);
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void updateNotExist() {
         storage.update(RESUME_4);
     }
 }
