150c00eba1f4fbc6f6be75f6ec501c8d14f70bcb
AlekPo <al0do@yandex.ru>
2019-12-23 19:47:41 +0300

    Lesson14 -JOIN

--- src/ru/javaops/basejava/storage/SqlStorage.java
+++ src/ru/javaops/basejava/storage/SqlStorage.java
@@ -1,12 +1,14 @@
 package ru.javaops.basejava.storage;
 
 import ru.javaops.basejava.exception.NotExistStorageException;
+import ru.javaops.basejava.model.ContactType;
 import ru.javaops.basejava.model.Resume;
 import ru.javaops.basejava.sql.SqlHelper;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SqlStorage implements Storage {
     private final SqlHelper sqlHelper;
@@ -26,20 +28,31 @@ public class SqlStorage implements Storage {
 
     @Override
     public Resume get(String uuid) {
-        String strSql = "SELECT * FROM resume WHERE resume.uuid =?";
+        String strSql = "" +
+                " SELECT * FROM resume r " +
+                "   LEFT JOIN contact c " +
+                "     ON r.uuid = c.resume_uuid " +
+                "  WHERE r.uuid =?";
         return sqlHelper.executiveInterface(strSql, ps -> {
             ps.setString(1, uuid);
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 throw new NotExistStorageException(uuid);
             }
-            return new Resume(uuid, rs.getString("full_name"));
+            Resume resume = new Resume(uuid, rs.getString("full_name"));
+            do {
+                String value = rs.getString("value");
+                ContactType type = ContactType.valueOf(rs.getString("type"));
+                resume.setContact(type, value);
+            } while (rs.next());
+            return resume;
         });
     }
 
     @Override
     public void update(Resume resume) {
         String strSql = "UPDATE resume SET full_name = ? WHERE uuid = ?";
+        String strSql2 = "UPDATE resume ";
         sqlHelper.executiveInterface(strSql, ps -> {
             ps.setString(1, resume.getFullName());
             ps.setString(2, resume.getUuid());
@@ -59,6 +72,17 @@ public class SqlStorage implements Storage {
             ps.execute();
             return null;
         });
+
+        strSql = "INSERT INTO contact (resume_uuid, type, value) VALUES (?,?,?)";
+        for (Map.Entry<ContactType, String> entry : resume.getContacts().entrySet()) {
+            sqlHelper.executiveInterface(strSql, ps -> {
+                ps.setString(1, resume.getUuid());
+                ps.setString(2, entry.getKey().name());
+                ps.setString(3, entry.getValue());
+                ps.execute();
+                return null;
+            });
+        }
     }
 
     @Override
