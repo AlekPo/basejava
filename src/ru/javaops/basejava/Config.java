3cd2dfda15faccaaf0882481a7b9577184f450a6
AlekPo <al0do@yandex.ru>
2020-02-07 01:41:55 +0300

    Lesson17 -DB_credentials_as_resources

--- src/ru/javaops/basejava/Config.java
+++ src/ru/javaops/basejava/Config.java
@@ -3,27 +3,26 @@ package ru.javaops.basejava;
 import ru.javaops.basejava.storage.SqlStorage;
 import ru.javaops.basejava.storage.Storage;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    //    private static final File PROPS = new File("config\\resumes.properties");
-    private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
+    //    private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
 
     private final String storageDir;
-    private final String dbUrl;
-    private final String dbUser;
-    private final String dbPassword;
+    //    private final String dbUrl;
+//    private final String dbUser;
+//    private final String dbPassword;
     private final Storage storage;
 
     public static Config get() {
         return INSTANCE;
     }
 
+/*
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
             Properties props = new Properties();
@@ -37,34 +36,47 @@ public class Config {
             throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
         }
     }
+*/
+
+    private Config() {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
+            Properties props = new Properties();
+            props.load(is);
+//            storageDir = new File(props.getProperty("storage.dir"));
+            storageDir = props.getProperty("storage.dir");
+            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid config file " + PROPS);
+        }
+    }
 
     public String getStorageDir() {
         return storageDir;
-    }
-
-    public String getDbUrl() {
-        return dbUrl;
-    }
-
-    public String getDbUser() {
-        return dbUser;
-    }
-
-    public String getDbPassword() {
-        return dbPassword;
     }
 
     public Storage getStorage() {
         return storage;
     }
 
-    private static File getHomeDir() {
-        String strHomeDir = System.getProperty("homeDir");
-        File homeDir = new File(strHomeDir == null ? "." : strHomeDir);
-        if (!homeDir.isDirectory()) {
-            throw new IllegalStateException(homeDir.getName() + "  is not directory");
-        }
-        return homeDir;
-    }
+//    public String getDbUrl() {
+//        return dbUrl;
+//    }
+//
+//    public String getDbUser() {
+//        return dbUser;
+//    }
+//
+//    public String getDbPassword() {
+//        return dbPassword;
+//    }
+//
 
+//    private static File getHomeDir() {
+//        String strHomeDir = System.getProperty("homeDir");
+//        File homeDir = new File(strHomeDir == null ? "." : strHomeDir);
+//        if (!homeDir.isDirectory()) {
+//            throw new IllegalStateException(homeDir.getName() + "  is not directory");
+//        }
+//        return homeDir;
+//    }
 }
